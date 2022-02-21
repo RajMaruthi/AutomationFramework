2e84e7092fe7e05882acafea586c0fd9db48c106
Maruthi <maruthirajabn@gmail.com>
2022-02-21 15:35:05 +0530

    Initial Commit...

---
+++ TraceI_Automation/src/org/honeywell/Trace/testcase_scripts/TraceDemo.java
@@ -0,0 +1,16 @@
+package org.honeywell.Trace.testcase_scripts;
+
+public class TraceDemo {
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+	
+	static void m1()
+	{
+		System.out.println("M1 method Demo");
+	}
+
+}
